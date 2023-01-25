0f53a6e8a624cb59d63f141419370f1c302ddf67
T4ngkeke <96550407+T4ngkeke@users.noreply.github.com>
2023-01-25 11:32:08 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,11 +1,14 @@
-class HomeProcess:
+from multiprocessing import Process
+import threading
+class HomeProcess(Process):
 
     def __init__(self, production_rate, consumption_rate, policy):
+        super().__init__()
         self.production_rate = production_rate
         self.consumption_rate = consumption_rate
         self.message_queue = []
     
-    def run_process(self):
+    def run (self):
         while True:
             # Receive messages from other home processes
             self.receive_messages()
@@ -18,30 +21,45 @@ class HomeProcess:
             # Sleep for a time period
             time.sleep(1)
 
-class MarketProcess:
+class MarketProcess(Process):
 
-    def __init__(self, current_price):
-        self.current_price = current_price
-        self.sockets = []
-        self.transaction_limit = 10
+    	def __init__(self, current_price):
+    		super().__init__()
+        	self.current_price = current_price
+        	self.sockets = []
+        	self.transaction_limit = 10
 
-    def run_process(self):
-        while True:
-            # Receive messages from home processes
-            self.receive_messages()
-            # Calculate energy price
-            self.calculate_energy_price()
-            # Update market display
-            self.update_market_display()
-            # Sleep for a time period
-            time.sleep(1)
+	def run (self):
+	        external=ExternalProcess()
+	        external.start()
+	        #external.join()
+	        def myMarket():
+	        	while True:
+	            # Receive messages from home processes
+				self.receive_messages()
+	            # Calculate energy price
+	            		self.calculate_energy_price()
+	            # Update market display
+	            		self.update_market_display()
+	            # Sleep for a time period
+	            		time.sleep(1)
+	        market1=threading.Thread(target=myMarket,args())
+	        market2=threading.Thread(target=myMarket,args())
+	        market3=threading.Thread(target=myMarket,args())
+	        market1.start()
+	        market2.start()
+	        market3.start()
+	        market1.join()
+	        market2.join()
+	        market3.join()
+	        external.join()
+	        
+	        
+	#def marketCalculate()	
             
             
             
 class ExternalProcess(Process):
-
-    def __init__(self, parent_process):
-        self.parent_process = parent_process
 
     def run(self):
         while True:
@@ -52,7 +70,44 @@ class ExternalProcess(Process):
             # Sleep for a time period
             time.sleep(1)                 
 
-class WeatherProcess:
+class WeatherProcess(Process):
 
-    def __init__(self):
-        self.shared_memory = {}
+	def __init__(self):
+        	super().__init__()
+        	self.shared_memory = {}
+	def run(self):
+		#à ajouter
+
+if __name__=="__main__":
+	home1=HomeProcess(productionRate1,consumptionRate1,policy)
+	home2=HomeProcess(productionRate2,consumptionRate2,policy)
+	home3=HomeProcess(productionRate3,consumptionRate3,policy)
+	
+	market=MarketProcess(currentPrice)
+	
+	weather=WeatherProcess()
+	
+	
+	home1.start()
+	home2.start()
+	home3.start()
+	
+	market.start()
+	
+	weather.start()
+	
+	
+	home1.join()
+	home2.join()
+	home3.join()
+	
+	market.join()
+	
+	weather.join()
+	
+	
+	
+	
+	
+
+
