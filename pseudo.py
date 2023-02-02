729a0303f2994b55ce038d49ac620dfefd334994
T4ngkeke <96550407+T4ngkeke@users.noreply.github.com>
2023-02-02 22:20:14 +0100

    Update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -4,3 +4,24 @@ def signalHandler:
 def signalChild:
     50% probability
         send SIGUSR1 to market! 
+ 
+
+
+
+class WeatherProcess:
+    send the temperature to main
+    
+class MarketProcess:
+    use socket to connect with clients
+    start a random event by signal
+    calculate the price of this round 
+    
+class homeProcess:
+    receive temperature from main
+    trade energy with other homes
+    send its energy to market
+
+main:
+    start all processes
+    print all results
+    
